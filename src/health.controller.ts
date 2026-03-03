1ccaf68081b41c71e49353d2fb9eff784d430595
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2026-03-03 09:22:32 -0500

    salud

--- src/health.controller.ts
+++ src/health.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 import { BusinessResponse } from '@common/decorators';
 import { SuccessCodes } from '@common/constants';
 
-@Controller('salud')
+@Controller()
 export class HealthController {
   @Get()
+  @Redirect('salud', 302)
+  root() {
+    return;
+  }
+
+  @Get('salud')
   @BusinessResponse(SuccessCodes.SALUD_200)
   verificar() {
     return;
